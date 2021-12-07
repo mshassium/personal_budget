3513238705bc38f1d3a42a16fc36891217c8200e
mshassium <mshassium@gmail.com>
2021-12-07 18:28:47 +0400

    remove vaadin relation

--- src/main/java/ru/mshassium/personalbudget/db/service/DailyTransactionService.java
+++ src/main/java/ru/mshassium/personalbudget/db/service/DailyTransactionService.java
@@ -1,30 +1,11 @@
 package ru.mshassium.personalbudget.db.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.vaadin.artur.helpers.CrudService;
-import ru.mshassium.personalbudget.db.entity.DailyTransactions;
 import ru.mshassium.personalbudget.db.repository.DailyTransactionRepository;
 
-import java.time.LocalDateTime;
-import java.util.List;
-
 @Service
-public class DailyTransactionService extends CrudService<DailyTransactions, Integer> {
+public class DailyTransactionService {
 
     private DailyTransactionRepository repository;
-
-    public DailyTransactionService(@Autowired DailyTransactionRepository repository) {
-        this.repository = repository;
-    }
-
-    @Override
-    protected DailyTransactionRepository getRepository() {
-        return repository;
-    }
-
-    public List<DailyTransactions> getAllForPeriod(LocalDateTime from, LocalDateTime to){
-        return repository.getAllForPeriod(from, to);
-    }
 
 }
